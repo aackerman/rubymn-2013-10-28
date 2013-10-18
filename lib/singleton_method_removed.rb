73269fdd11516dd90d80efcfd4cc27a43c553e16
Aaron Ackerman <theron17@gmail.com>
2013-10-17 20:50:38 -0500

    Added example for removing a singleton method

---
+++ lib/singleton_method_removed.rb
@@ -0,0 +1,9 @@
+module Shippable
+  def self.singleton_method_added(method_name)
+    puts "#{method_name} added to #{self.name} as a singleton method"
+  end
+
+  def self.shipit!; end
+
+  remove_method :shipit!
+end
